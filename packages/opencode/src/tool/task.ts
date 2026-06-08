panwar-stack/opencode

fix(opencode): align team wake with prompt ops

packages/opencode/src/tool/task.ts
@@ -17,6 +17,7 @@ export interface TaskPromptOps {
   cancel(sessionID: SessionID): Effect.Effect<void>
   resolvePromptParts(template: string): Effect.Effect<SessionPrompt.PromptInput["parts"]>
   prompt(input: SessionPrompt.PromptInput): Effect.Effect<MessageV2.WithParts>
+  wake(sessionID: SessionID): Effect.Effect<MessageV2.WithParts>
 }
 
 const id = "task"

panwar-stack/oc2

packages/opencode/src/tool/task.ts
@@ -19,6 +19,7 @@ export interface TaskPromptOps {
   cancel(sessionID: SessionID): Effect.Effect<void>
   resolvePromptParts(template: string): Effect.Effect<SessionPrompt.PromptInput["parts"]>
   prompt(input: SessionPrompt.PromptInput): Effect.Effect<SessionV1.WithParts>
+  wake(sessionID: SessionID): Effect.Effect<SessionV1.WithParts>
 }
 
 const id = "task"